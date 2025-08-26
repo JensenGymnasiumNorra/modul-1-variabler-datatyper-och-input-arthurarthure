1f9f8d5bd2a6fe01a86d6e6285956a642250dac7
arthurarthure <arthurmukiibi08@gmail.com>
2025-08-26 11:31:36 +0200

    Uppg3

--- uppg3_birthyear.py
+++ uppg3_birthyear.py
@@ -6,4 +6,11 @@ Exempel på utskrift, det inom () ska ändras om variablerna ändras:
 Du är (16) år gammal.
 
 """
+print("hur gammal är jag chat?")
 
+Birth_year = 2008
+current_year = 2025
+
+age= current_year - Birth_year
+
+print(f"Du är {age} år gammal")
